22564704abd57cca95a49bd2b7e93b79cf5ff4bc
Rakshita Devurkar <rdevurk1@binghamton.edu>
2017-11-29 23:31:54 -0500

    Validations for Email added

--- submit/web-auth/index.js
+++ submit/web-auth/index.js
@@ -15,6 +15,7 @@ const https = require('https');
 
 //local dependencies
 const users = require('./users/users');
+const options = require('./options').options;
 
 const STATIC_DIR = 'statics';
 const TEMPLATES_DIR = 'templates';
@@ -45,41 +46,43 @@ function registerUserHandler(app) {
       res.send(doMustache(app, 'register', {}));
     }
     else {
-      ['fname', 'lname', 'email', 'pwd', 'cpwd'].forEach((name) => {
-      const val = req.query[name];
-      if (typeof val === 'undefined' || val.trim().length === 0) {
-        const temp = name+"Error";
-        errorObj[temp] = 'Please provide a value';
-        errCount++;
-      }
-    });
-      if(req.query.pwd !== req.query.cpwd) {
-        errorObj['cpwdError'] = 'The passwords do not match';
-        errCount++;
-      }
-      //Validate Email
-      //Validate Password
+        ['fname', 'lname', 'email', 'pwd', 'cpwd'].forEach((name) => {
+        const val = req.query[name];
+        if (typeof val === 'undefined' || val.trim().length === 0) {
+          const temp = name+"Error";
+          errorObj[temp] = 'Please provide a value';
+          errCount++;
+        }
+      });
+        if(req.query.pwd !== req.query.cpwd) {
+          errorObj['cpwdError'] = 'The passwords do not match';
+          errCount++;
+        }
+        if(!/.+@.+/.test(req.query.email)) {
+          errorObj['emailError'] = 'Please provide a valid email address';
+          errCount++;
+        }
         if(errCount > 0) {
-          errorObj['fname'] = req.query.fname; errorObj['lname'] = req.query.lname; errorObj['email'] = req.query.email;
+            errorObj['fname'] = req.query.fname; errorObj['lname'] = req.query.lname; errorObj['email'] = req.query.email;          
           res.send(doMustache(app, 'register', errorObj));
           errCount=0;
           errorObj = {};
         }
         else {
-        app.users.registerUser(req.query)
-        .then((result) => {
-          if(result.status == 201) {
-            res.cookie('USER_COOKIE',result.name, {path: '/'});
-            res.send(doMustache(app, 'account', {name:result.name}));
-          }
-          else if(result.status == 303) {
-            const errorMsg = {'qError': 'This user already exists. Please provide a different email address','fname':req.query.fname, 'lname':req.query.lname, 'email':req.query.email};
-            res.send(doMustache(app, 'register', errorMsg));
-          }
-        })
-        .catch((err) => {
-          console.error(err);
-        });
+          app.users.registerUser(req.query,options.wsUrl)
+          .then((result) => {
+            if(result.status == 201) {
+              res.cookie('USER_COOKIE',result.name);
+              res.send(doMustache(app, 'account', {name:result.name}));
+            }
+            else if(result.status == 303) {
+              const errorMsg = {'qError': 'This user already exists. Please provide a different email address','fname':req.query.fname, 'lname':req.query.lname, 'email':req.query.email};
+              res.send(doMustache(app, 'register', errorMsg));
+            }
+          })
+          .catch((err) => {
+            console.error(err);
+          });
         }
       }
   }
@@ -87,9 +90,7 @@ function registerUserHandler(app) {
 
 function loginUserHandler(app) {
   return function(req,res) {
-    console.log("Inside login");
-    console.log(req.cookies.USER_COOKIE);
-    if(typeof req.cookies.USER_COOKIE === 'undefined' || req.cookies.USER_COOKIE == {}) {
+    if(typeof req.cookies.USER_COOKIE === 'undefined' || req.cookies.USER_COOKIE == '') {
       const email = req.query.email;
     const pwd = req.query.pwd;
     const isDisplay = (typeof req.query.submit === 'undefined');
@@ -101,16 +102,19 @@ function loginUserHandler(app) {
           const msg = {'email': req.query.email, 'pwd':req.query.pwd, 'qError': 'Please provide all the values'};
             res.send(doMustache(app, 'login', msg));
       }
-      //check for valid email
+      else if(!/.+@.+/.test(req.query.email)) {
+          const msg =  {'email': req.query.email, 'pwd':req.query.pwd, 'qError': 'Please provide a valid email address'};
+          res.send(doMustache(app, 'login', msg));
+      }
       else {
-        app.users.loginUser(req.query)
+        app.users.loginUser(req.query,options.wsUrl)
         .then((result) => {
           if(result.status == 200) {
-            app.users.getUser(email,result.authToken)
+            app.users.getUser(email,result.authToken,options.wsUrl)
             .then((userData) => {
               if(userData.data) {
                 const userName = userData.data.fname+" "+userData.data.lname;
-                res.cookie('USER_COOKIE',userName,{path: '/'});
+                res.cookie('USER_COOKIE',userName);
                 res.send(doMustache(app, 'account', {name:userName}));
               }
               else if(userData.status == 401) {
@@ -138,8 +142,8 @@ function loginUserHandler(app) {
 
 function logoutUserHandler(app) {
   return function(req, res) {
-    console.log("Entering logout");
-    res.clearCookie(USER_COOKIE,{path: '/'});
+    res.cookie('USER_COOKIE','');
+    // res.clearCookie(USER_COOKIE,{path: '/'});
     res.redirect('/login');
   };
 }
@@ -149,15 +153,6 @@ function logoutUserHandler(app) {
 
 
 /************************ Utility functions ****************************/
-
-function getPort(argv) {
-  let port = null;
-  if (argv.length !== 3 || !(port = Number(argv[2]))) {
-    console.error(`usage: ${argv[1]} PORT`);
-    process.exit(1);
-  }
-  return port;
-}
 
 function doMustache(app, templateId, view) {
   // const templates = { footer: app.templates.footer };
@@ -185,15 +180,13 @@ function setupTemplates(app) {
 }
 
 function setup() {
+  const app = express();
   process.chdir(__dirname);
   process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-  const app = express();
-  const port = 3000;
+  app.locals.port = options.port;
   const certOptions = {
-  // key: fs.readFileSync(sslDir+'/key.pem'),
-  // cert: fs.readFileSync(sslDir+'/cert.pem')
-  key: fs.readFileSync('/home/dchopde1/Desktop/certificates/proj4/key.pem'),
-  cert: fs.readFileSync('/home/dchopde1/Desktop/certificates/proj4/cert.pem')
+  key: fs.readFileSync(options.sslDir+'/key.pem'),
+  cert: fs.readFileSync(options.sslDir+'/cert.pem')
   };
   app.users=users;
   app.use(express.static(STATIC_DIR));
@@ -201,7 +194,7 @@ function setup() {
   app.use(cookieParser());
   setupTemplates(app);
   setupRoutes(app);
-  https.createServer(certOptions, app).listen(port);
+  https.createServer(certOptions, app).listen(app.locals.port);
   console.log("Listening");
 }
 
--- submit/web-auth/users/users.js
+++ submit/web-auth/users/users.js
@@ -3,17 +3,18 @@
 const axios = require('axios');
 
 
-const WS_URL = 'https://localhost:443';
+// const WS_URL = 'https://localhost:443';
 
 function Users() {
-  this.baseUrl = WS_URL;
+  
 }
 
-Users.prototype.registerUser = function(reqQuery) {
+Users.prototype.registerUser = function(reqQuery,baseUrl) {
+    console.log("Inside registerUser");
     const ID = reqQuery.email;
     const pwd = reqQuery.pwd;
     const data = Object.assign({}, {'fname': reqQuery.fname, 'lname':reqQuery.lname});
-    return axios.put(`${this.baseUrl}/users/${ID}?pw=${pwd}`, data, { maxRedirects: 0 })
+    return axios.put(`${baseUrl}/users/${ID}?pw=${pwd}`, data, { maxRedirects: 0 })
       .then((response) => {
         const resObj = {"status":response.status, "name":reqQuery.fname+" "+reqQuery.lname};
         return resObj;
@@ -24,10 +25,10 @@ Users.prototype.registerUser = function(reqQuery) {
       });
 }
 
-Users.prototype.loginUser = function(reqQuery) {
+Users.prototype.loginUser = function(reqQuery,baseUrl) {
   const email = reqQuery.email;
   const data = Object.assign({}, {'pw':reqQuery.pwd});
-  return axios.put(`${this.baseUrl}/users/${email}/auth`, data, { maxRedirects: 0 })
+  return axios.put(`${baseUrl}/users/${email}/auth`, data, { maxRedirects: 0 })
       .then((response) => {
         return {"status":response.status, "authToken":response.data.authToken};
       })
@@ -37,9 +38,9 @@ Users.prototype.loginUser = function(reqQuery) {
       });
 }
 
-Users.prototype.getUser = function(email, authToken) {
+Users.prototype.getUser = function(email, authToken,baseUrl) {
   const AuthStr = 'Bearer '.concat(authToken);
-  return axios.get(`${this.baseUrl}/users/${email}`, {headers: { Authorization: AuthStr }})
+  return axios.get(`${baseUrl}/users/${email}`, {headers: { Authorization: AuthStr }})
     .then((response) => {
       return {data: response.data};
     })
